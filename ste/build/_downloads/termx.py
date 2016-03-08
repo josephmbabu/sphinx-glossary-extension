e5e2ba6bdebea41870ba11cc2fae04c7653fc3ba
ajpII <anthony.petrillo@gmail.com>
2016-03-08 18:22:11 -0500

    random cleanup

--- ste/build/_downloads/termx.py
+++ ste/build/_downloads/termx.py
@@ -1,4 +1,5 @@
 from docutils import nodes
+import re
 
 class termx(nodes.Admonition, nodes.Element):
     pass
@@ -43,6 +44,7 @@ class TermxDirective(Directive):
                              self.content, self.lineno, self.content_offset,
                              self.block_text, self.state, self.state_machine)
 
+        # termx_all_termxs must be set to True in config.py
         if not hasattr(env, 'termx_all_termxs'):
             env.termx_all_termxs = []
         env.termx_all_termxs.append({
@@ -61,6 +63,7 @@ def purge_termxs(app, env, docname):
                           if termx['docname'] != docname]
 
 def process_termx_nodes(app, doctree, fromdocname):
+    print "Is calling proccess."
     if not app.config.termx_include_termxs:
         for node in doctree.traverse(termx):
             node.parent.remove(node)
@@ -74,8 +77,11 @@ def process_termx_nodes(app, doctree, fromdocname):
             node.replace_self([])
             continue
 
+        # The array to store all the termxs accross the site
+        # Structure: [term_info_1, term_reference_1, term_info_2, term_reference_2, ... term_info_n, term_reference_n]
         content = []
 
+        # Loop through each termx in the site environment
         for termx_info in env.termx_all_termxs:
             para = nodes.paragraph()
             filename = env.doc2path(termx_info['docname'], base=None)
@@ -95,11 +101,60 @@ def process_termx_nodes(app, doctree, fromdocname):
             para += newnode
             para += nodes.Text('.)', '.)')
 
+            # The data to enter into the termxlist array
+            input_list = [termx_info['termx'], para]
+
+            # Add the input to the termxlist in order
+            content = __insert_in_order(input_list, content)
+
             # Insert into the termxlist
-            content.append(termx_info['termx'])
-            content.append(para)
+            # content.append(termx_info['termx'])
+            # content.append(para)
+            
+        for item2 in content:
+            title = __get_title_from_termx_info(item2.__str__())
+            if len(title) > 0:
+                print title[0]
 
         node.replace_self(content)
+
+# Insert a termx into a termxlist in order
+def __insert_in_order(the_input, the_list):
+    new_list =[]
+    is_inserted = False
+
+    print "Entered list:"
+    for i in xrange(len(the_list)):
+        print "\t",i,":",the_list[i]
+
+    for item in xrange(len(the_list)):
+        if the_list[item].__str__().find('<termx') != -1:
+            input_title = __get_title_from_termx_info(the_input[0].__str__())[0]
+            item_title = __get_title_from_termx_info(the_list[item].__str__())[0]
+            if item_title > input_title:
+                # Insert our entry into content
+                new_list = the_list[:item] + the_input +  the_list[item:]
+                print input_title,"is less than",item_title,"so:"
+                for i in xrange(len(new_list)):
+                    print "\t",i,":",new_list[i]
+                is_inserted = True
+                break
+
+    if not is_inserted:
+        new_list = the_list
+        new_list += the_input
+        print "End of list, so:"
+        for i in xrange(len(new_list)):
+            print "\t",i,":",new_list[i]
+
+    print "Added: " + __get_title_from_termx_info(the_input[0].__str__())[0]
+
+    return new_list
+
+# Search the title of the termx object
+def __get_title_from_termx_info(info):
+    return re.findall('(?<=<title>)(.*?)(?=<\/title>)', info)
+
 
 def setup(app):
     app.add_config_value('termx_include_termxs', False, 'html')
